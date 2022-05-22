bd6b3f490b5a5cf65d465c54fe33c19df08bb237
soeunkk <luckyber1@naver.com>
2022-05-22 18:21:54 +0900

    templates 경로 변경

--- myblog/blogapp/views.py
+++ myblog/blogapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'blogapp/home.html')
 
 def community(request):
-    return render(request, 'community.html')
+    return render(request, 'blogapp/community.html')
